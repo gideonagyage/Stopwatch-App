86a56f6a7f9e754f072d7b16b8f3ff95797f6449
Gideon Agyage <gideonagyage@gmail.com>
2024-08-06 11:43:02 +0000

    Second commit - Finalize initailization

---
+++ src/Stopwatch.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Stopwatch () {
+    return (
+        <div>
+            <h1>Stopwatch</h1>
+        </div>
+    )
+}
+
+export default Stopwatch;
